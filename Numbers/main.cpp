4dabb7ff962db4307105232e7730b69b76e8cb73
xXTgamerXx9844 <trinethr_n@protonmail.com>
2021-05-24 13:53:20 +0530

    Added My Files

---
+++ Numbers/main.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+  int myNum = 5;
+  double myDecimal = 3.10284097235;
+  double mySum = myNum+myDecimal;
+  cout << "Number is- " << myNum+myDecimal << endl;
+  cout << "LOL numbers" <<endl;
+  cout << "" <<endl;
+  cout << "Sum-" << mySum;
+  
+  return 0;
+} 
+
+
